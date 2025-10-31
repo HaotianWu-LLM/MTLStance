2cb5f327f84c243670c694292dfd092a2c107b67
HaotianWu-LLM <wuhaotian0a1@gmail.com>
2025-10-31 15:57:28 +0800

    Add files via upload

---
+++ Trident/run_vast.py
@@ -0,0 +1,61 @@
+import os
+import socket
+
+if __name__ == '__main__':
+    data = 'vast'
+    batch_size = 4
+    epochs = 50
+    patience = 10
+    lr = 2e-5
+    l2_reg = 5e-5
+    shared_model = 'bert-base'
+    wiki_model = 'bert-base'
+    google_model = 'bert-base'
+    tweet_model = 'bertweet'
+    n_layers_freeze = 0
+    n_layers_freeze_wiki = 0
+    gpu = '0'
+    inference = 0
+
+    if wiki_model == shared_model:
+        n_layers_freeze_wiki = n_layers_freeze
+    if not wiki_model or wiki_model == shared_model:
+        n_layers_freeze_wiki = 0
+
+    os.makedirs('results', exist_ok=True)
+    if data != 'vast':
+        file_name = f'results/{data}-lr={lr}-bs={batch_size}.txt'
+    else:
+        file_name = f'results/{data}-lr={lr}-bs={batch_size}.txt'
+
+    if shared_model != 'bert-base':
+        file_name = file_name[:-4] + f'-{shared_model}.txt'
+    if n_layers_freeze > 0:
+        file_name = file_name[:-4] + f'-n_layers_fz={n_layers_freeze}.txt'
+    if wiki_model:
+        file_name = file_name[:-4] + f'-wiki={wiki_model}.txt'
+    if n_layers_freeze_wiki > 0:
+        file_name = file_name[:-4] + f'-n_layers_fz_wiki={n_layers_freeze_wiki}.txt'
+
+    n_gpus = len(gpu.split(','))
+    file_name = file_name[:-4] + f'-n_gpus={n_gpus}.txt'
+
+    command = f"python3 -u src/train.py " \
+              f"--data={data} " \
+              f"--shared_model={shared_model} " \
+              f"--wiki_model={wiki_model} " \
+              f"--tweet_model={tweet_model} " \
+              f"--google_model={google_model} " \
+              f"--n_layers_freeze={n_layers_freeze} " \
+              f"--n_layers_freeze_wiki={n_layers_freeze_wiki} " \
+              f"--batch_size={batch_size} " \
+              f"--epochs={epochs} " \
+              f"--patience={patience} " \
+              f"--lr={lr} " \
+              f"--l2_reg={l2_reg} " \
+              f"--gpu={gpu} " \
+              f"--inference={inference} " \
+              # f" > {file_name}"
+
+    print(command)
+    os.system(command)
